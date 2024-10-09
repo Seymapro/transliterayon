890631a3c47e24f91eaadf2ed0d4bee670f073f4
Seymapro <86776895+Seymapro@users.noreply.github.com>
2024-10-09 22:45:16 +0300

    Add files via upload

--- transliterate.py
+++ transliterate.py
@@ -1,56 +1,55 @@
 ru_to_tr = {
-    "а": "a",
-    "б": "b",
-    "в": "v",
-    "г": "g",
-    "д": "d",
-    "е": "e",
-    "ё": "yo",
-    "ж": "j",
-    "з": "z",
-    "и": "i",
-    "й": "y",
-    "к": "k",
-    "л": "l",
-    "м": "m",
-    "н": "n",
-    "о": "o",
-    "п": "p",
-    "р": "r",
-    "с": "s",
-    "т": "t",
-    "у": "u",
-    "ф": "f",
-    "х": "h",
-    "ц": "ts",
-    "ч": "ç",
-    "ш": "ş",
-    "щ": "şç",
-    "ъ": "",
-    "ы": "ı",
-    "ь": "",
-    "э": "e",
-    "ю": "yu",
-    "я": "ya",
+    ("А", "а"): ("A", "a"),
+    ("Б", "б"): ("B", "b"),
+    ("В", "в"): ("V", "v"),
+    ("Г", "г"): ("G", "g"),
+    ("Д", "д"): ("D", "d"),
+    ("Е", "е"): ("Ye", "ye"),
+    ("Ё", "ё"): ("Yo", "yo"),
+    ("Ж", "ж"): ("J", "j"),
+    ("З", "з"): ("Z", "z"),
+    ("И", "и"): ("İ", "i"),
+    ("Й", "й"): ("Y", "y"),
+    ("К", "к"): ("K", "k"),
+    ("Л", "л"): ("L", "l"),
+    ("М", "м"): ("M", "m"),
+    ("Н", "н"): ("N", "n"),
+    ("О", "о"): ("O", "o"),
+    ("П", "п"): ("P", "p"),
+    ("Р", "р"): ("R", "r"),
+    ("С", "с"): ("S", "s"),
+    ("Т", "т"): ("T", "t"),
+    ("У", "у"): ("U", "u"),
+    ("Ф", "ф"): ("F", "f"),
+    ("Х", "х"): ("H", "h"),
+    ("Ц", "ц"): ("Ts", "ts"),
+    ("Ч", "ч"): ("Ç", "ç"),
+    ("Ш", "ш"): ("Ş", "ş"),
+    ("Щ", "щ"): ("Şç", "şç"),
+    ("Ъ", "ъ"): ("'", "'"),
+    ("Ы", "ы"): ("I", "ı"),
+    ("Ь", "ь"): ("\u005E", "\u005E"),
+    ("Э", "э"): ("E", "e"),
+    ("Ю", "ю"): ("Yu", "yu"),
+    ("Я", "я"): ("Ya", "ya"),
 }
 
+
 tr_to_ru = {value: key for key, value in ru_to_tr.items()}
-del tr_to_ru[""]
 
 
-def transliterate_ru(text: str):
-    text = text.lower()
-    for ru, tr in ru_to_tr.items():
-        text = text.replace(ru, tr)
+def transliterate(text: str):
+    russian_chars = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+
+    if any(char in russian_chars for char in text):
+        for (ru_upper, ru_lower), (tr_upper, tr_lower) in ru_to_tr.items():
+            text = text.replace(ru_upper, tr_upper).replace(ru_lower, tr_lower)
+    else:
+        for (tr_upper, tr_lower), (ru_upper, ru_lower) in tr_to_ru.items():
+            text = text.replace(tr_upper, ru_upper).replace(tr_lower, ru_lower)
+
     return text
 
 
-def transliterate_tr(text: str):
-    text = text.lower()
-    for tr, ru in tr_to_ru.items():
-        text = text.replace(tr, ru)
-    return text
-
-
-print(transliterate_ru("На улице светит солнце, и птицы поют веселые песни."))
-print(transliterate_tr("orospu çocuğu vakıfbank"))
+print(transliterate("На улице светит солнце, и птицы поют веселые песниь."))
+print(transliterate("orospu çocuğu vakıfbank"))
